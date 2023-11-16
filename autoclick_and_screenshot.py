91ca4e9ab7881ddc2e61f1fe223ef9118715498d
Owen Yi <owenyi333@gmail.com>
2023-11-17 09:46:03 +1100

    deleting some old files

--- autoclick_and_screenshot.py
+++ autoclick_and_screenshot.py
@@ -12,8 +12,3 @@ for i in range(NUM_FRAMES):
 
     im = pyautogui.screenshot()
     im.save(f'bad_apple_output/frame{i:04}.png')
-
-# note: turn off afterwards?
-# System Settings > Privacy & Security > Screen Recording > Visual Studio Code
-# System Settings > Privacy & Security > Accessibility > Visual Studio Code 
-# System Settings > Desktop and Dock > Menu Bar > Automatically Hide and show the Menu Bar Never to -> in full screen only
